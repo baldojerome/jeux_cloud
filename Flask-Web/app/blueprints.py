a155aa6db3fee335dcfd45595287669025463f50
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-12-11 12:37:42 -0500

    Delete Flask-Web directory

--- Flask-Web/app/blueprints.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-import controller as c
-
-main = Blueprint('main',__name__, url_prefix="/")
-
-main.route("/",                                 methods=['GET'])            (c.main_index)
-main.route("/login",                            methods=['GET', 'POST'])    (c.main_login)
-main.route("/logout",                           methods=['GET'])            (c.main_logout)
-main.route('/register',                         methods=['GET','POST'])     (c.user_create)
